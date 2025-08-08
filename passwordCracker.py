4037568c917d06c5fe61db67b4b653446d8ddbd5
Zedonng <sprogramacion940@gmail.com>
2025-08-08 10:31:40 -0500

    Inittial Commit

--- passwordCracker.py
+++ passwordCracker.py
@@ -1,10 +1,20 @@
 import zipfile
 
-zip_file = 'Secret.zip'
+zip_file = "Secret.zip"
+
+if (zip_file == ""):
+    print(f"No se encontro el archivo {zip_file}")
+else:
+    pass
 
 diccionario = "rockyou.txt"
 
-with zipfile.ZipFile(zip_file, 'r') as zf:
+if (diccionario == ""):
+    print(f"No se encontro el diccionario {diccionario}")
+else:
+    pass
+
+with zipfile.ZipFile(zip_file, 'r') as f:
     with open(diccionario, 'r', errors='ignore') as f:
         for linea in f:
              password = linea.strip()
@@ -13,4 +23,4 @@ with zipfile.ZipFile(zip_file, 'r') as zf:
                  print(f"Contraseña Crackeada: {password}")
                  break
              except:
-                 pass
+                 print(f"La contraseña no pudo ser crackeada: {NameError}")
